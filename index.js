de46ddbd19992c8233700e8346b0018bb91dd039
Smile Thanapattheerakul <th.thanyathorn@gmail.com>
2021-06-18 17:33:12 -0400

    debug

--- index.js
+++ index.js
@@ -320,6 +320,7 @@ async function getBHGenerate() {
     });
 
     const result = await response.text();
+    console.log(result);
     generateStory.innerHTML = result.text + '...';
   } catch (error) {
     generateStory.innerHTML = 'Seems like there is no story for this caption. Down to try again 🧐';
