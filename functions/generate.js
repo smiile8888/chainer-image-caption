8136a9dc6f352e1832ad46c817acd0490f2a3b37
Smile Thanapattheerakul <th.thanyathorn@gmail.com>
2021-06-18 17:12:35 -0400

    update generate

--- functions/generate.js
+++ functions/generate.js
@@ -1,10 +1,21 @@
 const axios = require('axios');
-require('dotenv').config();
+// const fetch = require('node-fetch');
+// require('dotenv').config();
 
 const key = process.env.COHERE_API_KEY
 
 const handler = async (event) => {
     try {
+        // const response = await fetch('https://api.cohere.ai/baseline-shark/generate', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': `Bearer ${key}`,
+        //     },
+        //     body: event.body,
+        // });
+
+        // const result = await response.json();
         const response = await axios({
             method: 'post',
             url: 'https://api.cohere.ai/baseline-shark/generate',
