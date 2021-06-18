5f7b694d9b5e0aa4a5521ff995d7e6a0d0c653ef
Smile Thanapattheerakul <th.thanyathorn@gmail.com>
2021-06-18 17:38:53 -0400

    clean up generate

--- functions/generate.js
+++ functions/generate.js
@@ -1,42 +1,29 @@
 const axios = require('axios');
-// const fetch = require('node-fetch');
-// require('dotenv').config();
 
-const key = process.env.COHERE_API_KEY
+const API_KEY = process.env.COHERE_API_KEY;
 
 const handler = async (event) => {
     try {
-        // const response = await fetch('https://api.cohere.ai/baseline-shark/generate', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Authorization': `Bearer ${key}`,
-        //     },
-        //     body: event.body,
-        // });
-
-        // const result = await response.json();
         const response = await axios({
             method: 'post',
             url: 'https://api.cohere.ai/baseline-shark/generate',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${key}`
+                'Authorization': `Bearer ${API_KEY}`
             },
             data: event.body
-        })
+        });
 
         return {
             statusCode: response.status,
             body: JSON.stringify(response.data),
-        }
+        };
     } catch (error) {
-        let status = error && error.response ? error.response.status : 500
         return {
-            statusCode: status,
+            statusCode: error && error.response ? error.response.status : 500,
             body: error.message,
-        }
-    }
-}
+        };
+    };
+};
 
-module.exports = { handler }
+module.exports = { handler };
